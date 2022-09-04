b32d55d07c97169724f786f65589b5673f940ae2
fcsgdh <3368741433@qq.com>
2022-09-04 22:24:12 +0800

    工厂模式

--- src/org/ali/strategy/Comparable.java
+++
@@ -1,9 +0,0 @@
-package org.ali.strategy;
-
-/**
- * Author: lury
- * Date: 2022-09-04 10:51
- */
-public interface Comparable<T> {
-    int CompareTo(T o);
-}
--- src/org/ali/strategy/Comparator.java
+++
@@ -1,9 +0,0 @@
-package org.ali.strategy;
-
-/**
- * Author: lury
- * Date: 2022-09-04 12:44
- */
-public interface Comparator<T> {
-    int compare(T o1,T o2);
-}
